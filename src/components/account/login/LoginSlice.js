cd1dd4a86c2e1366243fccaa972e3c70fcfcd1e3
Mark Hryt <markhrytchak@gmail.com>
2023-07-19 13:15:00 -0400

    cookies not set on frontend

--- src/components/account/login/LoginSlice.js
+++ src/components/account/login/LoginSlice.js
@@ -5,13 +5,7 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const login = createAsyncThunk('auth/login', async (userData) => {
   try {
     const {response} = await axios.post(BASE_URL+'/login',
-    {email: userData.email, password: userData.password}, {withCredentials: true}).then((response)=>{
-      const sessionId = response.data.sessionId;
-      document.cookie = `sessionId=${sessionId}; domain=splendid-tuxedo-frog.cyclic.app; secure; sameSite=none; max-age=${60 * 60 * 24}; path=/;`;
-
-      // Set the sessionId in a cookie
-    })
-
+    {email: userData.email, password: userData.password}, {withCredentials: true});
     return response.data;
   } catch (error) {
     throw Error(error.message); 
@@ -20,17 +14,9 @@ export const login = createAsyncThunk('auth/login', async (userData) => {
 
 export const isUserLoggedIn = createAsyncThunk('auth/isLoggedIn', async () => {
   try {
-    let response = await fetch(BASE_URL+'/isLoggedIn',
-    {
-      credentials: 'include'
-    })
-    // let response = await axios.get(BASE_URL+'/isLoggedIn',
-    // {
-    //   withCredentials: true,
-    //  });
-    let data = await response.json();
-    console.log(data)
-    return data;
+    let response = await axios.get(BASE_URL+'/isLoggedIn',
+    {withCredentials:true});
+    return response.data;
   } catch (error) {
     throw Error(error.message);
   }
