468fffe62244e5241bc0d78b5d520c4a2690bf9f
markhryt <hrytchakmark@gmail.com>
2023-07-21 13:14:32 -0400

    base url added to placeorder slice

--- src/components/PlaceOrder/PlaceOrderSlice.js
+++ src/components/PlaceOrder/PlaceOrderSlice.js
@@ -1,13 +1,14 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 // Async thunk that makes a POST request to the /placeorder route with credentials
 export const placeOrder = createAsyncThunk('placeOrder/post', async () => {
   try {
     let cart=JSON.parse(sessionStorage.getItem('cart')).items;
-    const response = await axios.post('/placeorder',
-    { withCredentials: true },
+    const response = await axios.post(BASE_URL+'/placeorder',
     {cart: cart},
+    { withCredentials: true },
     );
     return response.data;
   } catch (error) {
