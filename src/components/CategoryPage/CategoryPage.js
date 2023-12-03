3b84ada5e9800b69f5c269bfe15eaac5917ff84d
markhryt <hrytchakmark@gmail.com>
2023-12-03 11:13:08 -0500

    Category Page Improved and Home SideBars added

--- src/components/CategoryPage/CategoryPage.js
+++ src/components/CategoryPage/CategoryPage.js
@@ -3,7 +3,7 @@ import { useParams, useLocation } from "react-router"
 import Products from "../products/Products";
 import { useDispatch } from "react-redux";
 import { fetchProductsByCategory } from "../ProductsList/ProductListSlice";
-
+import "./CategoryPage.css"
 export default function CategoryPage(){
     const { id } = useParams();
     const location = useLocation();
@@ -16,7 +16,7 @@ export default function CategoryPage(){
 
     return(
         <div>
-            <h1>{ category}</h1>
+            <h1 className="text-center mb-5 categoryPage">{ category}</h1>
             <Products/>
         </div>
     )
